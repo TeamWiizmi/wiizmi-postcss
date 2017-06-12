07f15604c3e9f4d7b9e3fe226d7fabfe4a625d9e
Jent Zheng <jent.zheng@gmail.com>
2017-06-12 23:09:51 +0800

    add custom typography mixin

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,27 @@
 module.exports = {
   plugins: [
     require('postcss-import')(),
-    require('postcss-modular-scale')(),
-    require('postcss-lh')(),
+    require('postcss-nested'),
+    require('postcss-mixins')(),
     require('postcss-cssnext')(),
-    require('postcss-utilities')(),
-    require('postcss-nested')(),
+    require('postcss-functions')({
+      functions: {
+        modularScale: function(scale, ratio) {
+          ModularScale = require('modular-scale');
+          var msScale = new ModularScale({ratios: [ratio], bases: [1]});
+          return msScale(scale) + 'rem';
+        }
+      }
+    }),
+    require('postcss-lh'),
+    require('postcss-utilities'),
+    require('postcss-extend'),
+    require('cssnano')({
+      preset: ['default', {
+        discardComments: {
+          removeAll: true,
+        },
+      }]
+    }),
   ]
 }
